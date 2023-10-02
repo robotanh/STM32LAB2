548779b8309846948a87a84cee3ca27b7c19039b
robotanh <robotanh15@gmail.com>
2023-10-02 16:12:44 +0700

    init LAB2

---
+++ STM_LAB2/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 2, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void set_Timer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
